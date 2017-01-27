a8bf0f057d31cc11f52ffbb0a984d7f44897d761
Roshan Ravi <roshanravi10@gmail.com>
2017-01-27 08:21:05 -0500

    Add little documentation

--- example.js
+++ example.js
@@ -1,5 +1,7 @@
 const tba = require("tba-js");
 
+// Needs to follow this pattern:
+// - `<team/person id>:<app description>:<version>`
 tba.setAppID("frc4909:tba-api:v0.0.1");
 
 tba.getMatches("2016", "casd", (data) =>{
