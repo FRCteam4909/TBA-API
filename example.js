4526f254ffca748ec22e1ab52382ace5e90e11e0
Roshan Ravi <roshanravi10@gmail.com>
2017-01-27 08:17:37 -0500

    Change Example Event Code

--- example.js
+++ example.js
@@ -2,6 +2,6 @@ const tba = require("tba-js");
 
 tba.setAppID("frc4909:tba-api:v0.0.1")
 
-tba.getMatches("2010", "sc", (data) =>{
+tba.getMatches("2016", "casd", (data) =>{
     console.dir(data);
 });
